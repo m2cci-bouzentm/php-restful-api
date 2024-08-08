1df22d38234286327ae2935165aebd594b2c9d63
Mohamed <mohakatalonya10@gmail.com>
2024-08-08 18:15:44 +0200

    trying __DIR__

--- backend/routes/album.php
+++ backend/routes/album.php
@@ -1,22 +1,20 @@
 <?php
 
-
 $main_uri = '/';
 
 // $router = new Router();
 
 // get all albums
-$router->get($main_uri . 'albums', 'controllers/album/getAlbums.php');
-
+$router->get($main_uri . 'albums', __DIR__ . '/controllers/album/getAlbums.php');
 
 // get one album by id
-$router->get($main_uri . 'album', 'controllers/album/getAlbum.php');
+$router->get($main_uri . 'album', __DIR__ . '/controllers/album/getAlbum.php');
 
-// add a album
-$router->post($main_uri . 'album', 'controllers/album/addAlbum.php');
+// add an album
+$router->post($main_uri . 'album', __DIR__ . '/controllers/album/addAlbum.php');
 
-// delete a album by id
-$router->delete($main_uri . 'album', 'controllers/album/destroyAlbum.php');
+// delete an album by id
+$router->delete($main_uri . 'album', __DIR__ . '/controllers/album/destroyAlbum.php');
 
-// update a album by id
-$router->put($main_uri . 'album', 'controllers/album/updateAlbum.php');
+// update an album by id
+$router->put($main_uri . 'album', __DIR__ . '/controllers/album/updateAlbum.php');
