de04010946239f5d0b94f1a85c6ae7f2513634c5
Mohamed <mohakatalonya10@gmail.com>
2024-08-09 01:02:24 +0200

    front end deployment

--- backend/controllers/band/getBands.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: application/json");
-
-$response = json_encode(Band::getAllBands());
-
-echo $response;
--- backend/routes/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-$main_uri = '/php-restful-api/backend/';
-
-// $router = new Router();
-
-$router->get($main_uri . '', 'controllers/index.php');
