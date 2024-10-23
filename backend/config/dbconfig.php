38bc2c3880a9e7fe3e32c08c6912b13b78ebfcc3
m2cci-bouzentm <mohakatalonya10@gmail.com>
2024-10-23 13:44:22 +0200

    deployed to flyio using docker image

--- backend/config/dbconfig.php
+++ backend/config/dbconfig.php
@@ -1,12 +1,12 @@
 <?php
 
 
-// define('HOST', $_ENV['DB_HOST']);
-// define('DBNAME', $_ENV['DB_NAME']);
-// define('USER', $_ENV['DB_USER']);
-// define('PWD', $_ENV['123456']);
+define('HOST', $_ENV['DB_HOST']);
+define('DBNAME', $_ENV['DB_NAME']);
+define('USER', $_ENV['DB_USER']);
+define('PASSWORD', $_ENV['PASSWORD']);
 
-define('HOST', 'db-php-restfulapi.cnuk4uooyk59.eu-north-1.rds.amazonaws.com');
-define('DBNAME', 'record_company');
-define('USER', 'root');
-define('PWD', 12345678);
+// define('HOST', 'db-php-restfulapi.cnuk4uooyk59.eu-north-1.rds.amazonaws.com');
+// define('DBNAME', 'record_company');
+// define('USER', 'root');
+// define('PASSWORD', 12345678);
--- backend/core/Database.php
+++ backend/core/Database.php
@@ -4,14 +4,15 @@ class Database
 {
 
   private static $connection = null;
-  private static $dsn = 'mysql:host=' . HOST . ';dbname=' . DBNAME;
-
+  // private static $dsn = 'mysql:host=' . HOST . ';dbname=' . DBNAME;
+  private static $dsn = 'pgsql:host=' . HOST . ';dbname=' . DBNAME . ';sslmode=require';
 
   protected static function getConnection()
   {
     if (self::$connection === null) {
       try {
-        self::$connection = new PDO(self::$dsn, USER, PWD);
+        self::$connection = new PDO(self::$dsn, USER, PASSWORD);
+        // self::$connection->setAttribute(PDO::ATTR_SERVER_INFO);
         self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch (PDOException $e) {
